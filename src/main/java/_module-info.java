8f0cd1259041ceab00cb00dc7df38c22961a741f
Lykke Bernberg <lykber01@easv365.dk>
2025-01-06 13:47:20 +0100

    tilføjet dbconnector

--- src/main/java/_module-info.java
+++ src/main/java/_module-info.java
@@ -1,8 +1,13 @@
+/*
 module dk.easv.pmc {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
+    requires com.microsoft.sqlserver.jdbc;
 
     opens dk.easv.pmc to javafx.fxml;
     exports dk.easv.pmc;
 }
+
+ */
