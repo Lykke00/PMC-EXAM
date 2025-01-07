06018a9f6494b97001987fb408a8eb6a81ae8bff
Lykke Bernberg <lykber01@easv365.dk>
2025-01-07 13:03:01 +0100

    identiteter

--- src/main/java/dk/easv/pmc/be/Movie.java
+++ src/main/java/dk/easv/pmc/be/Movie.java
@@ -1,7 +1,14 @@
 package dk.easv.pmc.be;
 
+import java.sql.Date;
+
 public class Movie {
     private int id;
+    private String name;
+    private double IMDBrating;
+    private double personalRating;
+    private String fileLink;
+    private Date lastView;
 
     public Movie(int id) {
         this.id = id;
@@ -9,5 +16,49 @@ public class Movie {
 
     public int getId() {
         return id;
+    }
+
+    private void setId(int id) {
+        this.id = id;
+    }
+
+    private String getName() {
+        return name;
+    }
+
+    private void setName(String name) {
+        this.name = name;
+    }
+
+    private double getIMDBrating() {
+        return IMDBrating;
+    }
+
+    private void setIMDBrating(double IMDBrating) {
+        this.IMDBrating = IMDBrating;
+    }
+
+    private double getPersonalRating() {
+        return personalRating;
+    }
+
+    private void setPersonalRating(double personalRating) {
+        this.personalRating = personalRating;
+    }
+
+    private String getFileLink() {
+        return fileLink;
+    }
+
+    private void setFileLink(String fileLink) {
+        this.fileLink = fileLink;
+    }
+
+    private Date getLastView() {
+        return lastView;
+    }
+
+    private void setLastView(Date lastView) {
+        this.lastView = lastView;
     }
 }
