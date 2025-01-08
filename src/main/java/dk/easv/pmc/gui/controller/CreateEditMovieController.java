012cc7fbdde64a9d6301781369193849cb974c47
Casper <casfro01@easv365.dk>
2025-01-08 10:13:06 +0100

    Alerts

--- src/main/java/dk/easv/pmc/gui/controller/CreateEditMovieController.java
+++ src/main/java/dk/easv/pmc/gui/controller/CreateEditMovieController.java
@@ -1,11 +1,17 @@
 package dk.easv.pmc.gui.controller;
 
+import dk.easv.pmc.gui.model.MovieModel;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 
-public class CreateEditMovieController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CreateEditMovieController{
+    private MovieModel mm;
 
 
     @FXML
@@ -23,8 +29,14 @@ public class CreateEditMovieController {
     @FXML
     private TextField txtFilePath;
 
+    public void setMovieModel(MovieModel movieModel){
+        this.mm = movieModel;
+    }
+
     @FXML
     private void saveMovie(ActionEvent actionEvent) {
+        String title = txtTitle.getText().trim();
+        if (title.it)
     }
 
     @FXML
