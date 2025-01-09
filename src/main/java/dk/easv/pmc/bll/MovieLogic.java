cf929f93749b97e1c5c2be523dc773c3f5d9147f
Lykke Bernberg <lykber01@easv365.dk>
2025-01-09 22:19:29 +0100

    påbegyndt inlæsning af alle film

--- src/main/java/dk/easv/pmc/bll/MovieLogic.java
+++ src/main/java/dk/easv/pmc/bll/MovieLogic.java
@@ -4,6 +4,8 @@ import dk.easv.pmc.be.Movie;
 import dk.easv.pmc.dal.dao.IMovieDAO;
 import dk.easv.pmc.dal.dao.MovieDAO;
 
+import java.util.ArrayList;
+
 public class MovieLogic {
     private final IMovieDAO movieDAO;
 
@@ -17,5 +19,9 @@ public class MovieLogic {
 
     public Movie createMovie(Movie movie) throws Exception {
         return this.movieDAO.createMovie(movie);
+    }
+
+    public ArrayList<Movie> getAllMovies() throws Exception {
+        return this.movieDAO.getAllMovies();
     }
 }
